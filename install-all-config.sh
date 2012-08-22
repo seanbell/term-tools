a4380a43d66668800e3fe5e0f5a5f2dd73df1959
Sean Bell <sbell@cs.cornell.edu>
2012-08-22 11:01:33 -0400

    Autojump: added wrapper around autojump to better handle failure cases

--- install-all-config.sh
+++ install-all-config.sh
@@ -5,11 +5,22 @@ set -x
 ./install-vim-config.sh $@
 
 # install autojump
+echo "Installing autojump into $HOME..."
+SRC_AUTOJUMP_BASH=~/.autojump/etc/profile.d/autojump.bash
+TMP_AUTOJUMP_BASH=~/.autojump-term-tools.tmp
 cd autojump
-./install.sh
+./install.sh > /dev/null
+cd ..
+sed 's/^function j {$/function j_impl {/' < $SRC_AUTOJUMP_BASH > $TMP_AUTOJUMP_BASH
+mv -f $TMP_AUTOJUMP_BASH $SRC_AUTOJUMP_BASH
+echo "Done"
 
 # this will fail if it already exists, so we are safe
 ln $@ -s ~/term-tools/config/gitconfig ~/.gitconfig
 ln $@ -s ~/term-tools/config/tmux.conf ~/.tmux.conf
 
 echo "run with -f to overwrite dotfiles"
+echo ""
+echo "Add this line to your bashrc:"
+echo "    [[ -f ~/term-tools/config/bashrc.sh ]] && source ~/term-tools/config/bashrc.sh"
+echo ""
