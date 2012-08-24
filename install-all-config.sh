2058c6b209525d09c10aa58e2f4983d468fce34e
Sean Bell <sbell@cs.cornell.edu>
2012-08-24 16:39:49 -0400

    Updated installer

--- install-all-config.sh
+++ install-all-config.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+# if -f, make sure it is intended
+for f in $@; do
+	if [ "$f" == "-f" ]; then
+		echo "Note: -f was provided as an argument, which may overwrite any existing configuration"
+		read -r -p "Are you sure? [y/N] " response
+		case $response in
+			[yY][eE][sS]|[yY])
+				:
+				;;
+			*)
+				exit 1
+				;;
+		esac
+	else
+		echo "Error: unknown argument: $f"
+		exit 1
+	fi
+done
+
+# trap errors
+function print_err() {
+	echo "ERROR: install incomplete"
+}
+trap print_err ERR
+
 set -x -e
 
 # install vim setup
@@ -23,15 +49,32 @@ echo "Done"
 ln $@ -s ~/term-tools/config/gitconfig ~/.gitconfig
 ln $@ -s ~/term-tools/config/tmux.conf ~/.tmux.conf
 
+# install oh-my-zsh config if it exists
+if [ -d ~/.oh-my-zsh ]; then
+	cd ~/.oh-my-zsh
+
+	if [ -d custom.backup ]; then
+		rm -rf custom
+	else
+		mv custom custom.backup
+	fi
+
+	ln $@ -s ~/term-tools/oh-my-zsh-custom custom
+	ln $@ -s ~/term-tools/config/sbell.zsh-theme themes/sbell.zsh-theme
+	cd -
+
+	# set theme and add syntax plugin
+	sed -E -e 's/ZSH_THEME=.*$/ZSH_THEME="sbell"/' -e 's/plugins=\((.*)\)/plugins=(\1 zsh-syntax-highlighting)/' \
+		-i .before-sbell ~/.zshrc
+fi
+
 set +x
 echo ""
 echo " == INSTALL COMPLETE =="
 echo ""
-echo "run with -f to overwrite dotfiles"
+echo "To overwrite existing config, rerun with -f (this will delete any existing configuration)"
 echo ""
-echo "Add this line to your bashrc:"
+echo "Add this line to your bashrc or zshrc:"
 echo "    [[ -f ~/term-tools/config/shrc.sh ]] && source ~/term-tools/config/shrc.sh"
 echo ""
-echo "To have nice c++ reformatting, install astyle:"
-echo "    sudo apt-get install astyle"
 echo " ======================"
