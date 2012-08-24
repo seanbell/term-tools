63346fe56b7344964f86f11177fd944da090663a
Sean Bell <sbell@cs.cornell.edu>
2012-08-24 17:28:47 -0400

    Ported config from ~/common to ~/term-tools

--- config/shrc.sh
+++ config/shrc.sh
@@ -35,3 +35,57 @@ if command -v dircolors >/dev/null 2>&1; then
 	eval $(dircolors $TERM_TOOLS_DIR/dircolors-solarized/dircolors.ansi-dark)
 fi
 
+# vim by default instead of emacs
+set -o vi
+export EDITOR="vim"
+# enable core dumps
+ulimit -c unlimited 2>/dev/null
+
+# store more shell history
+export HISTSIZE=1000000
+export HISTFILESIZE=1000000
+
+# syntax highlighting in less
+if [ -e /usr/share/source-highlight/src-hilite-lesspipe.sh ]; then
+  export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+  export LESS=' -R '
+fi
+
+# ZSH-SPECIFIC CONFIG
+if [ "$ZSH_VERSION" ]; then
+	# vim keybindings
+	bindkey -v
+	bindkey '\e[3~' delete-char
+	#bindkey '^R' history-incremental-search-backward
+	bindkey '^R' history-incremental-pattern-search-backward
+fi
+
+# BASH-SPECIFIC CONFIG
+if [ "$BASH_VERSION" ]; then
+
+	# Custom terminal: blue, 2 levels of directories, and git branch
+	export PROMPT_DIRTRIM=2
+	DEFAULT_COLOR="\[\e[0m\]"
+	PS1_COLOR="\[\e[1;32;40m\]"
+	GIT_COLOR="\[\e[34m\]"
+	TITLEBAR="\[\e]0;\h \w\007\]"
+	if command -v __git_ps1 >/dev/null 2>&1; then
+	  PS1="$TITLEBAR$PS1_COLOR\h:\w$GIT_COLOR\$(__git_ps1)$PS1_COLOR\$ $DEFAULT_COLOR"
+	else
+	  PS1="$TITLEBAR$PS1_COLOR\h:\w\$$DEFAULT_COLOR "
+	fi
+
+	# don't put duplicate lines or lines starting with space in the history.
+	export HISTCONTROL=ignoreboth
+
+	# don't clobber history
+	shopt -s histappend
+
+	# check the window size after each command and, if necessary,
+	# update the values of LINES and COLUMNS.
+	shopt -s checkwinsize
+
+	# automatically correct cd spelling errors
+	shopt -s cdspell
+
+fi
