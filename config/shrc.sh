85f1f97b4e5018c534258f348378b41fcac48aba
Sean Bell <sbell@cs.cornell.edu>
2012-08-26 14:33:34 -0400

    Reorganizing config

--- config/shrc.sh
+++ config/shrc.sh
@@ -4,9 +4,31 @@
 # directory containing these tools
 export TERM_TOOLS_DIR=~/term-tools
 
+# vim by default instead of emacs
+set -o vi
+export EDITOR="vim"
+
+# store more shell history
+export HISTSIZE=1000000
+export HISTFILESIZE=1000000
+
+# enable core dumps
+ulimit -c unlimited 2>/dev/null
+
 # 256 colors
 if [[ "$TERM" == "xterm" ]]; then
     export TERM="xterm-256color"
+fi
+
+# solarized color scheme
+if command -v dircolors >/dev/null 2>&1; then
+	eval $(dircolors $TERM_TOOLS_DIR/dircolors-solarized/dircolors.ansi-dark)
+fi
+
+# syntax highlighting in less
+if [ -e /usr/share/source-highlight/src-hilite-lesspipe.sh ]; then
+  export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+  export LESS=' -R '
 fi
 
 # autojump
@@ -15,51 +37,32 @@ if [ "$BASH_VERSION" ]; then
 	complete -F _cd j
 fi
 
-# call autojump, or fall back to cd if autojump fails
+# autojump wrapper (I've renamed "function j" in
+#   autojump.sh to "function j_impl")
 function j {
 	local _p=$PWD
 	j_impl $@
 
-	if [[ "$PWD" == "$_p" ]] && [[ -d "$1" ]]; then
+	if [[ "$PWD" == "$_p" ]] && [ -d "$1" ]; then
 		cd $1
 		echo -e "\\033[31m${PWD}\\033[0m"
 	fi
 }
 
-# solarized color scheme
-if command -v dircolors >/dev/null 2>&1; then
-	eval $(dircolors $TERM_TOOLS_DIR/dircolors-solarized/dircolors.ansi-dark)
-fi
-
-# vim by default instead of emacs
-set -o vi
-export EDITOR="vim"
-# enable core dumps
-ulimit -c unlimited 2>/dev/null
-
-# store more shell history
-export HISTSIZE=1000000
-export HISTFILESIZE=1000000
-
-# syntax highlighting in less
-if [ -e /usr/share/source-highlight/src-hilite-lesspipe.sh ]; then
-  export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-  export LESS=' -R '
-fi
-
 # ZSH-SPECIFIC CONFIG
 if [ "$ZSH_VERSION" ]; then
 
-	# ls after every cd
-	function chpwd() {
-		emulate -L zsh
-		ls
-	}
+	# ls after every cd (except when remotely logged in)
+	if [ ! -n "$SSH_CLIENT" ] && [ ! -n "$SSH_TTY" ]; then
+		function chpwd() {
+			emulate -L zsh
+			ls
+		}
+	fi
 
-	# vim keybindings
+	# vim keybindings for zsh
 	bindkey -v
 	bindkey '\e[3~' delete-char
-	#bindkey '^R' history-incremental-search-backward
 	bindkey '^R' history-incremental-pattern-search-backward
 
 fi
@@ -67,18 +70,18 @@ fi
 # BASH-SPECIFIC CONFIG
 if [ "$BASH_VERSION" ]; then
 
-	# ls after every cd (except on hydra)
-	if ! hostname | grep 'hydra' >/dev/null; then
+	# ls after every cd (except when remotely logged in)
+	if [ ! -n "$SSH_CLIENT" ] && [ ! -n "$SSH_TTY" ]; then
 		function cd()  {
 			 builtin cd "$@" && ls
 		}
 	fi
 
-	# Custom terminal: blue, 8 levels of directories, and git branch
-	export PROMPT_DIRTRIM=8
+	# Custom terminal: blue, 2 levels of directories, and git branch
+	export PROMPT_DIRTRIM=2
 	DEFAULT_COLOR="\[\e[0m\]"
-	PS1_COLOR="\[\e[0;38;40m\]"
-	GIT_COLOR="\[\e[34m\]"
+	PS1_COLOR="\[\e[34m\]"
+	GIT_COLOR="\[\e[33m\]"
 	TITLEBAR="\[\e]0;\h \w\007\]"
 	if command -v __git_ps1 >/dev/null 2>&1; then
 	  PS1="$TITLEBAR$PS1_COLOR\h:\w$GIT_COLOR\$(__git_ps1)$PS1_COLOR \$$DEFAULT_COLOR "
