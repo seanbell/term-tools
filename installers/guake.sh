6e4235db39ad8278d366e2f27b05df8af4094a7d
Sean Bell <sbell@cs.cornell.edu>
2012-08-26 13:59:28 -0400

    Added guake installer

---
+++ installers/guake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if command -v apt-get >/dev/null 2>&1; then
+	sudo apt-get install guake
+else
+	echo "ERROR: this installer only works for Ubuntu"
+	exit 1
+fi
+
+xmodmap -e "keycode 9 = F12"
+xmodmap -e "keycode 66 = Escape"
+xmodmap -pke > ~/.Xmodmap
+ln -s $@ ~/term-tools/config/xinitrc ~/.xinitrc
