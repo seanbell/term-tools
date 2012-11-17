173a08e5fbab7ed91014908d64fdac2b6e23d716
Sean Bell <sbell@cs.cornell.edu>
2012-11-16 23:12:17 -0500

    Improved gitconfig setup system

--- installers/git.sh
+++ installers/git.sh
@@ -19,7 +19,7 @@ FIX_GIT_USER=~/term-tools/fix-git-user.sh
 git config -l | grep "^user" | sed 's/^/git config --global /' | sed 's/=/ "/' | sed 's/$/"/' > $FIX_GIT_USER
 
 # use template
-mv -f ~/term-tools/config/gitconfig-template ~/term-tools/config/gitconfig
+cp -f ~/term-tools/config/gitconfig-template ~/term-tools/config/gitconfig
 
 # this will fail if it already exists, so we are safe
 ln $@ -s ~/term-tools/config/gitconfig ~/.gitconfig
