20a943958c087003e473bff503282dcdea117418
Sean Bell <sbell@cs.cornell.edu>
2013-01-31 11:35:59 -0500

    added installer for pip packages

--- installers/python.sh
+++ installers/python.sh
@@ -8,6 +8,16 @@ else
 	exit 1
 fi
 
+if command -v pip >/dev/null 2>&1; then
+	echo "pip exists"
+elif command -v apt-get >/dev/null 2>&1; then
+	sudo apt-get install -y python-pip
+else
+	echo "ERROR: pip is not installed"
+fi
+
+sudo pip install rope pep8 pylint
+
 ln $@ -s ~/term-tools/config/pythonrc ~/.pythonrc
 ln $@ -s ~/term-tools/config/inputrc ~/.inputrc
 
