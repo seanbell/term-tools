5469fa9a68b1169195586f19c46179e36b878efe
Sean Bell <sbell@cs.cornell.edu>
2013-12-30 09:44:26 -0500

    added ack

--- installers/vim.sh
+++ installers/vim.sh
@@ -3,18 +3,40 @@ set -e
 
 if [[ "$DESKTOP_SESSION" == "ubuntu" ]]; then
 	if command -v apt-get >/dev/null 2>&1; then
-		# vim-gnome integration
-		sudo apt-get install -y vim-gnome ctags
+		sudo apt-get install -y vim-gnome ctags ack-grep
 		# header for vim startify
 		sudo apt-get install -y fortune cowsay
+	else
+		echo "Cannot find apt-get"
+		exit 1
 	fi
-elif [[ $(vim --version | grep -c "+conceal") == "0" ]]; then
-	if command -v /opt/local/bin/port >/dev/null 2>&1; then
-		# macports
-		sudo port selfupdate
-		sudo port clean vim
-		sudo port install                       vim +big+python27+ruby
-		sudo port -n upgrade --enforce-variants vim +big+python27+ruby
+elif [ "$(uname)" == "Darwin" ]; then
+	if command -v ack >/dev/null 2>&1; then
+		echo "ack: installed"
+	else
+		if command -v brew >/dev/null 2>&1; then
+			brew install ack
+		elif command -v port >/dev/null 2>&1; then
+			sudo port install p5-app-ack
+		else
+			echo "Please install: ack-grep"
+			exit 1
+		fi
+	fi
+
+	if [[ $(vim --version | grep -c "+conceal") == "0" ]]; then
+		if command -v port >/dev/null 2>&1; then
+			# macports
+			sudo port selfupdate
+			sudo port clean vim
+			sudo port install                       vim +big+python27+ruby
+			sudo port -n upgrade --enforce-variants vim +big+python27+ruby
+		else
+			echo "Please install: vim (with +big, +python27, and +ruby)"
+			exit 1
+		fi
+	else
+		echo "vim: installed"
 	fi
 fi
 
