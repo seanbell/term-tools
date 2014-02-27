ec05fd5652cd1dc392f03a63e24a19d8d2d34384
Sean Bell <sbell@cs.cornell.edu>
2014-02-27 07:00:35 -0500

    added ack (previous commit message is incorrect)

---
+++ installers/ack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if command -v ack-grep >/dev/null 2>&1; then
+	echo "ack: exists"
+elif command -v apt-get >/dev/null 2>&1; then
+	# ubuntu
+	sudo apt-get install -y ack-grep
+elif command -v brew >/dev/null 2>&1; then
+	# homebrew
+	brew install ack
+else
+	echo "ERROR: ack is not installed"
+	exit 1
+fi
+
+ln $@ -s ~/term-tools/config/acrc ~/.ackrc
