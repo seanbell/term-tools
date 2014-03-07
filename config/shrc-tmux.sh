1d661e346725c962c67252382c41ef9b378bafa1
Sean Bell <sbell@cs.cornell.edu>
2014-03-07 10:08:01 -0500

    updated installer

--- config/shrc-tmux.sh
+++ config/shrc-tmux.sh
@@ -30,37 +30,40 @@ if [[ -s ~/.tmux.conf ]] && command -v tmux >/dev/null 2>&1; then
 
 		# make sure that this script is included last
 		line=$(grep -n -E 'source\s+.*config/shrc-tmux.sh' $rcfile | head -n 1 | awk -F: '{print $1}')
-		tot=$(wc -l < $rcfile)
-		let diff=$tot-$line
-		txt=$(tail -n $diff $rcfile)
+		if [[ ! -z "line" ]]; then
+			tot=$(wc -l < $rcfile)
+			let diff=$tot-$line
+			txt=$(tail -n $diff $rcfile)
 
-		if [[ ! $txt =~ [^[:space:]] ]] ; then
-			# this script is last
+			if [[ ! $txt =~ [^[:space:]] ]] ; then
+				# this script is last
 
-			# Now, check to make sure that we are in a fresh interactive non-tmux terminal
-			# (if not, fall through since we're already in a shell)
-			if [[ -z "$TMUX" ]] && [[ -n "$PS1" ]] && [[ "$TERM" == "xterm-256color" ]] && [[ $- = *i* ]]; then
-				# attach to dangling sessions if there are any
-				unattached=$(tmux ls | grep -E "^[0-9]+:" | grep -v "(attached)$" | head -n 1 | awk -F: '{print $1}')
-				if [[ -n "$unattached" ]]; then
-					echo "Re-attaching to session $unattached"
-					sleep 1
-					tmux -2 attach -t "$unattached" || tmux -2 new
-				else
-					tmux -2
+				# Now, check to make sure that we are in a fresh interactive non-tmux terminal
+				# (if not, fall through since we're already in a shell)
+				if [[ -z "$TMUX" ]] && [[ -n "$PS1" ]] && [[ "$TERM" == "xterm-256color" ]] && [[ $- = *i* ]]; then
+					# attach to dangling sessions if there are any
+					unattached=$(tmux ls | grep -E "^[0-9]+:" | grep -v "(attached)$" | head -n 1 | awk -F: '{print $1}')
+					if [[ -n "$unattached" ]]; then
+						echo "Re-attaching to session $unattached"
+						sleep 1
+						tmux -2 attach -t "$unattached" || tmux -2 new
+					else
+						tmux -2
+					fi
+
+					echo "tmux: exit $!"
 				fi
-
-				echo "tmux: exit $!"
+			else
+				echo -e "\e[1;31;48m"
+				echo "ERROR: In this file:"
+				echo "    $rcfile"
+				echo "There cannot be any commands after this line:"
+				echo "    source $this"
+				echo "Move all commands before this (line $line)."
+				echo -e "\e[m"
+				echo "This message is coming from $this."
 			fi
-		else
-			echo -e "\e[1;31;48m"
-			echo "ERROR: In this file:"
-			echo "    $rcfile"
-			echo "There cannot be any commands after this line:"
-			echo "    source $this"
-			echo "Move all commands before this (line $line)."
-			echo -e "\e[m"
-			echo "This message is coming from $this."
 		fi
+
 	fi
 fi
