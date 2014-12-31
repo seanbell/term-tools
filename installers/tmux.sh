7678110aa52987d3be2e262ffea0493e380f9abe
Sean Bell <sbell@cs.cornell.edu>
2014-12-30 20:59:48 -0500

    don't install teamocil

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -23,26 +23,26 @@ fi
 
 ln $@ -sn ~/term-tools/config/tmux.conf ~/.tmux.conf
 
-## teamocil
-
-if command -v teamocil >/dev/null 2>&1; then
-	echo "teamocil: exists"
-elif command -v gem >/dev/null 2>&1; then
-	sudo gem install teamocil
-elif command -v apt-get >/dev/null 2>&1; then
-
-	sudo apt-get install -y base-files
-	source /etc/os-release
-	if [[ ${VERSION%%.*} -ge 14 ]]; then
-		# Ubuntu 14.04
-		sudo apt-get install -y ruby
-	else
-		# Ubuntu 12.04
-		sudo apt-get install -y ruby1.9.3 rubygems
-	fi
-
-	sudo gem install teamocil
-else
-	echo "ERROR: rubygems is not installed"
-	exit 1
-fi
+### teamocil
+#
+#if command -v teamocil >/dev/null 2>&1; then
+#	echo "teamocil: exists"
+#elif command -v gem >/dev/null 2>&1; then
+#	sudo gem install teamocil
+#elif command -v apt-get >/dev/null 2>&1; then
+#
+#	sudo apt-get install -y base-files
+#	source /etc/os-release
+#	if [[ ${VERSION%%.*} -ge 14 ]]; then
+#		# Ubuntu 14.04
+#		sudo apt-get install -y ruby
+#	else
+#		# Ubuntu 12.04
+#		sudo apt-get install -y ruby1.9.3 rubygems
+#	fi
+#
+#	sudo gem install teamocil
+#else
+#	echo "ERROR: rubygems is not installed"
+#	exit 1
+#fi
