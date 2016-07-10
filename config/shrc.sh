9b662544a7b22affb9ca78af9583f32b727f979d
Sean Bell <sbell@grokstyle.com>
2016-07-10 03:18:03 +0000

    Removed autojump (I don't use it)

--- config/shrc.sh
+++ config/shrc.sh
@@ -43,12 +43,6 @@ fi
 # python autocomplete
 [[ -s ~/.pythonrc ]] && export PYTHONSTARTUP=~/.pythonrc
 
-# autojump
-[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source ~/.autojump/etc/profile.d/autojump.sh
-if [ "$BASH_VERSION" ]; then
-	complete -F _cd j
-fi
-
 # ack
 alias ack='ack-grep'
 alias sc='starcluster'
